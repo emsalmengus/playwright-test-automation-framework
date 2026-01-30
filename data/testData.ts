1e60dbb72810f0ac387873342960c132caed0fc2
emsalmengus <emsal.mengus@smail.th-koeln.de>
2026-01-30 21:36:11 +0100

    added testData

---
+++ data/testData.ts
@@ -0,0 +1,10 @@
+export const products = {
+    backpack: 'Sauce Labs Backpack',
+    bikeLight: 'Sauce Labs Bike Light'
+  };
+  
+  export const checkoutInfo = {
+    firstName: 'Max',
+    lastName: 'Mustermann',
+    postalCode: '50667'
+  };  
