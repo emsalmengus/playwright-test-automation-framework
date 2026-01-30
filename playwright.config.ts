a7501aee420ee793a3855f7791125b23ea1714b9
emsalmengus <emsal.mengus@smail.th-koeln.de>
2026-01-30 23:00:53 +0100

    excluded instable safari tests as well in config

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,12 +38,12 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
+/*
     {
       name: 'safari',
       use: { ...devices['Desktop Safari'] },
     },
-
+*/
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
